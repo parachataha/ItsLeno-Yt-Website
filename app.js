f65903d1b5591db205a21e06313d968c86ba6110
parachataha <paracahataha2@gmail.com>
2022-12-17 16:28:46 +0400

    3.1.0

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
 const close = document.querySelector('div.modal div.close');
 const modal = document.querySelector('div.modal');
 
-close.addEventListener('click', e => {
-    modal.style.display = 'none';
-})
+// close.addEventListener('close', e => {
+//     modal.style.display = 'none';  
+// })
+
+let newYear = new Date("1 Jan 2023");
+
+let daysElm = document.querySelector('div p span.days');
+let hoursElm = document.querySelector('div p span.hours');
+let minsElm = document.querySelector('div p span.mins');
+let secsElm = document.querySelector('div p span.secs');
+
+let countdownPage = document.querySelector('body#countdown');
+
+let daysElmP = document.querySelector('div p.days');
+let hoursElmP = document.querySelector('div p.hours');
+let minsElmP = document.querySelector('div p.mins');
+let secsElmP = document.querySelector('div p.secs');
+
+let fireworks = document.querySelector('section#countdown img')
+
+const beginCountdown = () => {
+    let currYear = new Date();
+    let diff = newYear - currYear;
+
+    let totalSecs = diff/ 1000;
+
+    let days = Math.floor(totalSecs / 3600 / 24);
+    let hours = Math.floor(totalSecs / 3600) % 24;
+    let mins = Math.floor(totalSecs / 60)% 60;
+    let secs = Math.floor(totalSecs) % 60;
+
+    // console.log(days, hours, mins, secs);
+
+    // days = 0;
+    // hours = 0;
+    // mins = 0;
+    // secs = 0;
+
+    daysElm.textContent = days;
+    hoursElm.textContent = hours;
+    minsElm.textContent = mins;
+    secsElm.textContent = secs;
+
+}
+
+beginCountdown();
+setInterval(beginCountdown, 1000);
+
+if (daysElm.textContent == 0 && hoursElm.textContent == 0 && minsElm.textContent == 0 && secsElm.textContent == 0) {
+    countdownPage.style.background = 'url("/assets/extras/fireworks-bg.gif")';
+    countdownPage.style.backgroundRepeat = 'no-repeat';
+    countdownPage.style.backgroundSize = 'cover';
+
+    daysElmP.style.background = 'rgb(255, 255, 255, 0.5)';
+    hoursElmP.style.background = 'rgb(255, 255, 255, 0.5)';
+    minsElmP.style.background = 'rgb(255, 255, 255, 0.5)';
+    secsElmP.style.background = 'rgb(255, 255, 255, 0.5)';
+
+    fireworks.style.display = 'block';
+}
